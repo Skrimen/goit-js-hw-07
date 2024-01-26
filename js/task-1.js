62bd3ddb9362d43e7a4992491bf4d0fd1540ad42
Skrimen <mokosin1991@gmail.com>
2024-01-26 22:35:12 +0200

    Yes

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,17 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const categoriesList = document.getElementById("categories");
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const categoryItems = categoriesList.querySelectorAll("li.item");
+
+console.log(`Кількість категорій: ${categoryItems.length}`);
+
+categoryItems.forEach((categoryItem) => {
+  const categoryTitle = categoryItem.querySelector("h2").textContent;
+
+  const categoryItemCount = categoryItem.querySelectorAll("ul > li").length;
+
+  console.log(`Категорія: ${categoryTitle}`);
+  console.log(
+    `Кількість елементів у категорії ${categoryTitle}: ${categoryItemCount}`
+  );
+  console.log("------------------------");
+});
