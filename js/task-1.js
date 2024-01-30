05be2fc94028ea50acac85131c5a4a827ca43d3d
Skrimen <mokosin1991@gmail.com>
2024-01-30 19:55:33 +0200

    ok

--- js/task-1.js
+++ js/task-1.js
@@ -2,16 +2,14 @@ const categoriesList = document.getElementById("categories");
 
 const categoryItems = categoriesList.querySelectorAll("li.item");
 
-console.log(`Кількість категорій: ${categoryItems.length}`);
+console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach((categoryItem) => {
   const categoryTitle = categoryItem.querySelector("h2").textContent;
 
   const categoryItemCount = categoryItem.querySelectorAll("ul > li").length;
 
-  console.log(`Категорія: ${categoryTitle}`);
-  console.log(
-    `Кількість елементів у категорії ${categoryTitle}: ${categoryItemCount}`
-  );
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements ${categoryItemCount}`);
   console.log("------------------------");
 });
